32cb5c6ee83a75dec425445883a294e4017f841b
Laboratorios <Laboratorios@LAB203-05>
2017-12-04 16:36:19 -0200

    Exibir as notas finais e faltas finais

---
+++ src/src/br/com/model/FaltaFinal.java
@@ -0,0 +1,231 @@
+package src.br.com.model;
+
+public class FaltaFinal {
+
+	private int ra_aluno;
+	private String nome_aluno;
+	private int data1;
+	private int data2;
+	private int data3;
+	private int data4;
+	private int data5;
+	private int data6;
+	private int data7;
+	private int data8;
+	private int data9;
+	private int data10;
+	private int data11;
+	private int data12;
+	private int data13;
+	private int data14;
+	private int data15;
+	private int data16;
+	private int data17;
+	private int data18;
+	private int data19;
+	private int data20;
+	private int data21;
+	private int data22;
+	private int totalFalta;
+
+	public int getRa_aluno() {
+		return ra_aluno;
+	}
+
+	public void setRa_aluno(int ra_aluno) {
+		this.ra_aluno = ra_aluno;
+	}
+
+	public String getNome_aluno() {
+		return nome_aluno;
+	}
+
+	public void setNome_aluno(String nome_aluno) {
+		this.nome_aluno = nome_aluno;
+	}
+
+	public int getData1() {
+		return data1;
+	}
+
+	public void setData1(int data1) {
+		this.data1 = data1;
+	}
+
+	public int getData2() {
+		return data2;
+	}
+
+	public void setData2(int data2) {
+		this.data2 = data2;
+	}
+
+	public int getData3() {
+		return data3;
+	}
+
+	public void setData3(int data3) {
+		this.data3 = data3;
+	}
+
+	public int getData4() {
+		return data4;
+	}
+
+	public void setData4(int data4) {
+		this.data4 = data4;
+	}
+
+	public int getData5() {
+		return data5;
+	}
+
+	public void setData5(int data5) {
+		this.data5 = data5;
+	}
+
+	public int getData6() {
+		return data6;
+	}
+
+	public void setData6(int data6) {
+		this.data6 = data6;
+	}
+
+	public int getData7() {
+		return data7;
+	}
+
+	public void setData7(int data7) {
+		this.data7 = data7;
+	}
+
+	public int getData8() {
+		return data8;
+	}
+
+	public void setData8(int data8) {
+		this.data8 = data8;
+	}
+
+	public int getData9() {
+		return data9;
+	}
+
+	public void setData9(int data9) {
+		this.data9 = data9;
+	}
+
+	public int getData10() {
+		return data10;
+	}
+
+	public void setData10(int data10) {
+		this.data10 = data10;
+	}
+
+	public int getData11() {
+		return data11;
+	}
+
+	public void setData11(int data11) {
+		this.data11 = data11;
+	}
+
+	public int getData12() {
+		return data12;
+	}
+
+	public void setData12(int data12) {
+		this.data12 = data12;
+	}
+
+	public int getData13() {
+		return data13;
+	}
+
+	public void setData13(int data13) {
+		this.data13 = data13;
+	}
+
+	public int getData14() {
+		return data14;
+	}
+
+	public void setData14(int data14) {
+		this.data14 = data14;
+	}
+
+	public int getData15() {
+		return data15;
+	}
+
+	public void setData15(int data15) {
+		this.data15 = data15;
+	}
+
+	public int getData16() {
+		return data16;
+	}
+
+	public void setData16(int data16) {
+		this.data16 = data16;
+	}
+
+	public int getData17() {
+		return data17;
+	}
+
+	public void setData17(int data17) {
+		this.data17 = data17;
+	}
+
+	public int getData18() {
+		return data18;
+	}
+
+	public void setData18(int data18) {
+		this.data18 = data18;
+	}
+
+	public int getData19() {
+		return data19;
+	}
+
+	public void setData19(int data19) {
+		this.data19 = data19;
+	}
+
+	public int getData20() {
+		return data20;
+	}
+
+	public void setData20(int data20) {
+		this.data20 = data20;
+	}
+
+	public int getData21() {
+		return data21;
+	}
+
+	public void setData21(int data21) {
+		this.data21 = data21;
+	}
+
+	public int getData22() {
+		return data22;
+	}
+
+	public void setData22(int data22) {
+		this.data22 = data22;
+	}
+
+	public int getTotalFalta() {
+		return totalFalta;
+	}
+
+	public void setTotalFalta(int totalFalta) {
+		this.totalFalta = totalFalta;
+	}
+
+}
---
+++ src/src/br/com/model/NotaFinal.java
@@ -0,0 +1,86 @@
+package src.br.com.model;
+
+public class NotaFinal {
+
+	private int ra_aluno;
+	private String nome_aluno;
+	private double n1;
+	private double n2;
+	private double trabalho;
+	private double pre_exame;
+	private double exame;
+	private double mediaFinal;
+	private String situacao;
+
+	public int getRa_aluno() {
+		return ra_aluno;
+	}
+
+	public void setRa_aluno(int ra_aluno) {
+		this.ra_aluno = ra_aluno;
+	}
+
+	public String getNome_aluno() {
+		return nome_aluno;
+	}
+
+	public void setNome_aluno(String nome_aluno) {
+		this.nome_aluno = nome_aluno;
+	}
+
+	public double getN1() {
+		return n1;
+	}
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+	public double getN2() {
+		return n2;
+	}
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+	public double getTrabalho() {
+		return trabalho;
+	}
+
+	public void setTrabalho(double trabalho) {
+		this.trabalho = trabalho;
+	}
+
+	public double getPre_exame() {
+		return pre_exame;
+	}
+
+	public void setPre_exame(double pre_exame) {
+		this.pre_exame = pre_exame;
+	}
+
+	public double getExame() {
+		return exame;
+	}
+
+	public void setExame(double exame) {
+		this.exame = exame;
+	}
+
+	public double getMediaFinal() {
+		return mediaFinal;
+	}
+
+	public void setMediaFinal(double mediaFinal) {
+		this.mediaFinal = mediaFinal;
+	}
+
+	public String getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(String situacao) {
+		this.situacao = situacao;
+	}
+}
